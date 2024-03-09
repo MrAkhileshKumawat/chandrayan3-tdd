76247fcf005d33aff45a5c351c099b106908d3f4
Akki k <akhilesh19@navgurukul.org>
2024-03-09 06:31:47 +0000

    Implement moving the spacecraft backward

--- src/spacecraft.js
+++ src/spacecraft.js
@@ -1,5 +1,71 @@
 function spacecraft(startingPosition, initialDirection, commands){
-    return { position: [0,1,0], direction: 'N' }
+    let [ x, y, z ] = startingPosition;
+    let direction = initialDirection;
+
+    for (let command of commands) {
+        switch (command) {
+            case 'f':
+                moveForward();
+                break;
+            case 'b':
+                moveBackward();
+                break;
+            default:
+                break;
+        }
+    }
+
+    function moveForward() {
+        switch (direction) {
+            case 'N':
+                y++;
+                break;
+            case 'S':
+                y--;
+                break;
+            case 'E':
+                x++;
+                break;
+            case 'W':
+                x--;
+                break;
+            case 'U':
+                z++;
+                break;
+            case 'D':
+                z--;
+                break;
+            default:
+                break;
+        }
+    }
+
+    function moveBackward() {
+        switch (direction) {
+            case 'N':
+                y--;
+                break;
+            case 'S':
+                y++;
+                break;
+            case 'E':
+                x--;
+                break;
+            case 'W':
+                x++;
+                break;
+            case 'U':
+                z--;
+                break;
+            case 'D':
+                z++;
+                break;
+            default:
+                break;
+        }
+    }
+
+    return { position: [x, y, z], direction }
 }
 
 module.exports = {spacecraft}
